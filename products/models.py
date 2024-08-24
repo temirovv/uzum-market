53f89a08301e21149d79bab33ba13cbf1e4a23c6
temirovv <temirovv21@gmail.com>
2024-08-24 17:29:24 +0500

    new models added

--- products/models.py
+++ products/models.py
@@ -3,8 +3,13 @@ from django.db.models import (
     CharField, ForeignKey, 
     CASCADE, ImageField,
     PositiveIntegerField, FloatField,
-    TextField
-    )
+    TextField, DateTimeField, BooleanField, DecimalField
+)
+from django.contrib.auth import get_user_model
+# from users.models import CustomUser
+
+
+User = get_user_model()
 
 
 class Category(Model):
@@ -48,3 +53,59 @@ class ProductImage(Model):
 class Color(Model):
     name = CharField(max_length=20)
     product = ForeignKey('products.Product', CASCADE, related_name='colors')
+
+
+# Cart models started
+
+class Cart(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    created_at = DateTimeField(auto_now_add=True)
+    is_active = BooleanField(default=True)
+
+    def __str__(self):
+        return f"Cart {self.id} for {self.user.username}"
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
+
+
+class CartItem(Model):
+    cart = ForeignKey(Cart, related_name='items', on_delete=CASCADE)
+    product = ForeignKey(Product, on_delete=CASCADE)
+    quantity = PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+
+    def total_price(self):
+        return self.quantity * self.product.price
+
+
+class Order(Model):
+    user = ForeignKey(User, related_name='orders', on_delete=CASCADE)
+    is_paid = BooleanField(default=False)
+    payment_method = CharField(max_length=255)
+    shipping_address = CharField(max_length=255)
+    total_price = DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+    def calculate_total_price(self):
+        self.total_price = sum(item.total_price() for item in self.items.all())
+        self.save()
+
+
+class OrderItem(Model):
+    order = ForeignKey(Order, related_name='items', on_delete=CASCADE)
+    product = ForeignKey(Product, on_delete=CASCADE)
+    quantity = PositiveIntegerField(default=1)
+    price = DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in order {self.order.id}"
+
+    def total_price(self):
+        return self.quantity * self.price
+
