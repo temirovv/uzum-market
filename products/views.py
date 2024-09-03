66130d4b1edade5171d52fdea13e00d11bbda17b
temirovv <temirovv21@gmail.com>
2024-09-03 18:29:10 +0500

    add product view updated

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 import json
 
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
-from .models import Product
+from .models import Product, Cart, CartItem
 
 
 def product_list(request):
@@ -23,11 +23,17 @@ def product_detail(request, id):
     return render(request, 'products/product-detail.html', context=context)
 
 
-@csrf_exempt
 def add_to_cart(request):
     if request.method == 'POST':
         print(request.body)
         data = json.loads(request.body)
+        product_id = data.get('product_id')
+        quantity = data.get('quantity')
 
+        product = get_object_or_404(Product, id=product_id)
+
+        cart, created = Cart.objects.get_or_create(user=request.user, is_active=True)
+        
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
 
     return JsonResponse(data={'status':'okey'})
