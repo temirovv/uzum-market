1d8a13ea26ce30cf4971f75adc684a5f64d71111
temirovv <temirovv21@gmail.com>
2024-10-26 17:22:50 +0500

    settings updated

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,7 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
